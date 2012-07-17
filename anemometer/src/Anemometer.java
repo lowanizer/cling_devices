1cf6e4521339956ba0e634038ab01fcbfa88f8a1
kevin <kevin@ubiquarium>
2012-07-17 12:22:02 +0200

    Ajout de l'anémomètre

---
+++ anemometer/src/Anemometer.java
@@ -0,0 +1,53 @@
+import org.teleal.cling.binding.annotations.*;
+
+import java.beans.PropertyChangeSupport;
+// Upnp Service are classes + annotations.
+
+import org.teleal.cling.binding.annotations.*;
+import java.beans.PropertyChangeSupport;
+import java.text.DecimalFormat;
+
+@UpnpService(
+        serviceId = @UpnpServiceId("Display"),
+        serviceType = @UpnpServiceType(value = "Anemometer", version = 1)
+)
+public class Anemometer {
+
+    private final PropertyChangeSupport propertyChangeSupport;
+
+    public Anemometer() {
+        this.propertyChangeSupport = new PropertyChangeSupport(this);
+    }
+
+    public PropertyChangeSupport getPropertyChangeSupport() {
+        return propertyChangeSupport;
+    }
+
+    @UpnpStateVariable(defaultValue = "0", sendEvents = true)
+    private int wind = 0;
+
+    @UpnpAction
+    public void setWind(@UpnpInputArgument(name = "NewTargetValue") int newTargetValue) {
+
+        int OldValue = wind;
+        wind = newTargetValue;
+
+	main.m_wind_display_label.set_text_message("Wind: "+newTargetValue+"km/h");
+
+	 // This will send a UPnP event, it's the name of a state variable that sends events
+    getPropertyChangeSupport().firePropertyChange("Wind", OldValue, wind);
+    }
+
+    @UpnpAction(out = @UpnpOutputArgument(name = "ResultWind"))
+    public int getWind() {
+        return wind;
+    }
+
+    @UpnpStateVariable(defaultValue="type=Display", sendEvents=true)
+    private String MetaData = "type=Display";
+
+    @UpnpAction(out=@UpnpOutputArgument(name="ResultMetaData"))
+    public String getMetaData()
+    { return MetaData; }
+
+}
---
+++ anemometer/src/main.java
@@ -0,0 +1,180 @@
+
+import org.teleal.cling.model.message.header.STAllHeader;
+import org.teleal.cling.model.meta.LocalDevice;
+import org.teleal.cling.model.meta.RemoteDevice;
+import org.teleal.cling.registry.Registry;
+import org.teleal.cling.registry.RegistryListener;
+import org.teleal.cling.UpnpService;
+import org.teleal.cling.UpnpServiceImpl;
+
+import org.teleal.cling.binding.annotations.AnnotationLocalServiceBinder;
+
+import org.teleal.cling.model.DefaultServiceManager;
+
+import org.teleal.cling.model.meta.DeviceDetails;
+import org.teleal.cling.model.meta.DeviceDetails;
+import org.teleal.cling.model.meta.DeviceIdentity;
+import org.teleal.cling.model.meta.Icon;
+import org.teleal.cling.model.meta.LocalService;
+import org.teleal.cling.model.meta.ManufacturerDetails;
+import org.teleal.cling.model.meta.ModelDetails;
+import org.teleal.cling.model.meta.ModelDetails;
+import org.teleal.cling.model.types.DeviceType;
+import org.teleal.cling.model.types.UDADeviceType;
+import org.teleal.cling.model.types.UDN;
+
+import org.teleal.cling.model.action.ActionExecutor;
+import org.teleal.cling.model.action.ActionInvocation;
+
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Graphics;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Dimension;
+
+import javax.swing.JProgressBar;
+import java.awt.Graphics2D;
+import java.awt.BorderLayout;
+
+import java.beans.PropertyChangeSupport;
+
+import java.awt.font.*;
+import java.awt.*;
+
+class wind_display_label extends javax.swing.JLabel
+{
+	public wind_display_label()
+	{
+		super(main.create_image_icon("anemometer.png", "Anemometer"));
+		m_text_message="";
+	}
+
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		if(m_text_message.equals("")) return;
+
+		Font f = new Font("SansSerif", Font.PLAIN, 35);
+		g.setFont(f);
+		FontMetrics fm=g.getFontMetrics();
+
+		final Dimension d=getSize();
+
+		final int line_height=fm.getHeight();
+		final int line_width=fm.stringWidth(m_text_message);
+
+		Graphics2D g2d=(Graphics2D)g;
+		g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+					     RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
+		g2d.drawString(m_text_message, (int)d.getWidth()/2-line_width/2,
+				(int)d.getHeight()/3);
+	}
+
+	public void set_text_message(String message)
+	{
+		m_text_message=message;
+		repaint();
+	}
+
+
+	private String m_text_message;
+
+}
+
+class main implements Runnable
+{
+	DefaultServiceManager<Anemometer> m_manager;
+	static wind_display_label m_wind_display_label;
+
+	// [Helper] Returns an image icon using the gui.getClass() or null 
+	// if the path was not valid
+	public static  ImageIcon create_image_icon(String path, String description) //{{{
+	{
+		java.net.URL img_url=main.class.getResource(path);
+		if(img_url!=null) return new ImageIcon(img_url, description);
+		else {
+			System.err.println("WARNING: couldn't find icon \""+path+"\"");
+			return null;
+		}
+
+	} //}}}
+
+	LocalDevice create_device() throws
+		Exception
+	{
+		DeviceIdentity identity = new DeviceIdentity(
+				UDN.uniqueSystemIdentifier("Demo Anemometer"));
+
+		DeviceType type=new UDADeviceType("Anemometer", 1);
+
+		DeviceDetails details=new DeviceDetails(
+				"Friendly Anemometer",
+				new ManufacturerDetails("ACME"),
+				new ModelDetails("Tornado 1900",
+					"A friendly Anemometer",
+					"v1"));
+			
+		LocalService<Anemometer> display_service=
+			(LocalService<Anemometer>)
+			new AnnotationLocalServiceBinder().read(Anemometer.class);
+
+		m_manager=new DefaultServiceManager<Anemometer>(display_service,
+				Anemometer.class);
+
+		display_service.setManager(m_manager);
+
+		return new LocalDevice(identity, type, details, display_service);
+
+	}
+
+	public static void main(String[] args)
+	{
+		Logger logger=Logger.getLogger("");
+		logger.setLevel(Level.SEVERE);
+
+		JFrame frame=new JFrame("Anemometer");
+		m_wind_display_label=new wind_display_label();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		frame.getContentPane().setBackground(java.awt.Color.white);
+		frame.getContentPane().add(m_wind_display_label, BorderLayout.CENTER);
+
+		frame.pack();
+		frame.setVisible(true);
+
+		Thread server_thread = new Thread (new main());
+		server_thread.setDaemon(false);
+		server_thread.start();
+	}
+
+	public void run()
+	{
+		try {
+			final UpnpService upnp_service=new UpnpServiceImpl();
+			
+			// setup cleanup at shutdown
+			Runtime.getRuntime().addShutdownHook(new Thread(){
+				@Override
+				public void run() {
+					upnp_service.shutdown();
+				}
+			});
+
+			// add the bound local device to the registry
+			upnp_service.getRegistry().addDevice(create_device());
+
+			int wind = 35;
+			m_manager.getImplementation().setWind(wind);
+		
+			
+		} catch(Exception ex){
+			System.err.println(ex);
+		}
+	}
+}
