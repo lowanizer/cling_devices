393ae186ae334a74ce437eb6f9bbc0c86ab0af5f
Anis Benyelloul <anis.benyelloul@gmail.com>
2012-08-02 09:58:54 +0200

    [phone] new device.

---
+++ phone/src/display.java
@@ -0,0 +1,62 @@
+import org.teleal.cling.binding.annotations.*;
+
+import java.beans.PropertyChangeSupport;
+// Upnp Service are classes + annotations.
+
+import org.teleal.cling.binding.annotations.*;
+import java.beans.PropertyChangeSupport;
+
+@UpnpService(
+        serviceId = @UpnpServiceId("Display"),
+        serviceType = @UpnpServiceType(value = "Display", version = 1)
+)
+public class display {
+
+    private final PropertyChangeSupport propertyChangeSupport;
+
+    public display() {
+        this.propertyChangeSupport = new PropertyChangeSupport(this);
+    }
+
+    public PropertyChangeSupport getPropertyChangeSupport() {
+        return propertyChangeSupport;
+    }
+
+    @UpnpStateVariable(defaultValue = "0", sendEvents = false)
+    private int status = 0;
+
+    @UpnpStateVariable(defaultValue = "0", sendEvents = false)
+    private String text = "";
+
+    @UpnpAction
+    public void setStatus(@UpnpInputArgument(name = "NewTargetValue") int newTargetValue) {
+
+        int statusOldValue = status;
+        status = newTargetValue;
+
+	main.m_tv_display_label.set_text_message("Status: "+newTargetValue);
+
+        // This will send a UPnP event, it's the name of a state variable that sends events
+        //getPropertyChangeSupport().firePropertyChange("Status", statusOldValue, status);
+    }
+
+    @UpnpAction
+    public void display(@UpnpInputArgument(name = "Text") String newTargetValue) {
+
+	main.m_tv_display_label.set_text_message(newTargetValue);
+
+    }
+
+    @UpnpAction(out = @UpnpOutputArgument(name = "ResultStatus"))
+    public int getStatus() {
+        return status;
+    }
+
+    @UpnpStateVariable(defaultValue="type=Display", sendEvents=true)
+    private String MetaData = "type=Display";
+
+    @UpnpAction(out=@UpnpOutputArgument(name="ResultMetaData"))
+    public String getMetaData()
+    { return MetaData; }
+
+}
---
+++ phone/src/main.java
@@ -0,0 +1,176 @@
+
+import org.teleal.cling.model.message.header.STAllHeader;
+import org.teleal.cling.model.meta.LocalDevice;
+import org.teleal.cling.model.meta.RemoteDevice;
+import org.teleal.cling.registry.Registry;
+import org.teleal.cling.registry.RegistryListener;
+import org.teleal.cling.UpnpService;
+import org.teleal.cling.UpnpServiceImpl;
+
+import org.teleal.cling.binding.annotations.AnnotationLocalServiceBinder;
+
+import org.teleal.cling.model.DefaultServiceManager;
+
+import org.teleal.cling.model.meta.DeviceDetails;
+import org.teleal.cling.model.meta.DeviceDetails;
+import org.teleal.cling.model.meta.DeviceIdentity;
+import org.teleal.cling.model.meta.Icon;
+import org.teleal.cling.model.meta.LocalService;
+import org.teleal.cling.model.meta.ManufacturerDetails;
+import org.teleal.cling.model.meta.ModelDetails;
+import org.teleal.cling.model.meta.ModelDetails;
+import org.teleal.cling.model.types.DeviceType;
+import org.teleal.cling.model.types.UDADeviceType;
+import org.teleal.cling.model.types.UDN;
+
+import org.teleal.cling.model.action.ActionExecutor;
+import org.teleal.cling.model.action.ActionInvocation;
+
+import java.util.logging.Logger;
+import java.util.logging.Level;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import java.awt.Graphics;
+import java.awt.Font;
+import java.awt.FontMetrics;
+import java.awt.Dimension;
+
+import javax.swing.JProgressBar;
+import java.awt.Graphics2D;
+import java.awt.BorderLayout;
+
+import java.beans.PropertyChangeSupport;
+
+import java.awt.font.*;
+import java.awt.*;
+
+class tv_display_label extends javax.swing.JLabel
+{
+	public tv_display_label()
+	{
+		super(main.create_image_icon("phone.png", "phone"));
+		m_text_message="";
+	}
+
+	protected void paintComponent(Graphics g)
+	{
+		super.paintComponent(g);
+		if(m_text_message.equals("")) return;
+
+		Font f = new Font("SansSerif", Font.PLAIN, 35);
+		g.setFont(f);
+		FontMetrics fm=g.getFontMetrics();
+
+		final Dimension d=getSize();
+
+		final int line_height=fm.getHeight();
+		final int line_width=fm.stringWidth(m_text_message);
+
+		Graphics2D g2d=(Graphics2D)g;
+		g2d.setRenderingHint(RenderingHints.KEY_TEXT_ANTIALIASING,
+					     RenderingHints.VALUE_TEXT_ANTIALIAS_ON);
+
+		g2d.drawString(m_text_message, (int)d.getWidth()/2-line_width/2,
+				(int)d.getHeight()/2);
+	}
+
+	public void set_text_message(String message)
+	{
+		m_text_message=message;
+		repaint();
+	}
+
+
+	private String m_text_message;
+
+}
+
+class main implements Runnable
+{
+	DefaultServiceManager<display> m_manager;
+	static tv_display_label m_tv_display_label;
+
+	// [Helper] Returns an image icon using the gui.getClass() or null 
+	// if the path was not valid
+	public static  ImageIcon create_image_icon(String path, String description) //{{{
+	{
+		java.net.URL img_url=main.class.getResource(path);
+		if(img_url!=null) return new ImageIcon(img_url, description);
+		else {
+			System.err.println("WARNING: couldn't find icon \""+path+"\"");
+			return null;
+		}
+
+	} //}}}
+
+	LocalDevice create_device() throws
+		Exception
+	{
+		DeviceIdentity identity = new DeviceIdentity(
+				UDN.uniqueSystemIdentifier("Demo Phone"+Math.random()));
+
+		DeviceType type=new UDADeviceType("phone", 1);
+
+		DeviceDetails details=new DeviceDetails(
+				"phone",
+				new ManufacturerDetails("ACME"),
+				new ModelDetails("TV 4000",
+					"A friendly Tv",
+					"v1"));
+			
+		LocalService<display> display_service=
+			(LocalService<display>)
+			new AnnotationLocalServiceBinder().read(display.class);
+
+		m_manager=new DefaultServiceManager<display>(display_service,
+				display.class);
+
+		display_service.setManager(m_manager);
+
+		return new LocalDevice(identity, type, details, display_service);
+
+	}
+
+	public static void main(String[] args)
+	{
+		Logger logger=Logger.getLogger("");
+		logger.setLevel(Level.SEVERE);
+
+		JFrame frame=new JFrame("Tv");
+		m_tv_display_label=new tv_display_label();
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+		frame.getContentPane().setBackground(java.awt.Color.white);
+		frame.getContentPane().add(m_tv_display_label, BorderLayout.CENTER);
+
+		frame.pack();
+		frame.setVisible(true);
+
+		Thread server_thread = new Thread (new main());
+		server_thread.setDaemon(false);
+		server_thread.start();
+	}
+
+	public void run()
+	{
+		try {
+			final UpnpService upnp_service=new UpnpServiceImpl();
+			
+			// setup cleanup at shutdown
+			Runtime.getRuntime().addShutdownHook(new Thread(){
+				@Override
+				public void run() {
+					upnp_service.shutdown();
+				}
+			});
+
+			// add the bound local device to the registry
+			upnp_service.getRegistry().addDevice(create_device());
+
+		} catch(Exception ex){
+			System.err.println(ex);
+		}
+	}
+}
